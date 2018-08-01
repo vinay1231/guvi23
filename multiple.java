70aaa774cc3eeab582bf12ff72bfe3388e2a4e6f
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-08-01 14:45:30 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n, c;
+    
+      Scanner in = new Scanner(System.in);
+      n = in.nextInt();
+   
+ 
+      for (c = 1; c <= 5; c++)
+         System.out.println(+ (n*c));
+	}
+}
